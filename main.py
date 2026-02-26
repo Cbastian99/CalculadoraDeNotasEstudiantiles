db1c907a4f356035c37427257abc1964c5bd267d
Sebastian Suarez <sebastiansuarezarias@gmail.com>
2026-02-26 14:43:32 -0500

    Mejoro calculos matematicos

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def calcular_promedio(notas):
+    return sum(notas) / len(notas)
+
+
 print("=== CALCULADORA DE NOTAS ESTUDIANTILES ===")
 
 nombre = input("Ingrese el nombre del estudiante: ")
@@ -6,7 +10,9 @@ nota1 = float(input("Ingrese la primera nota: "))
 nota2 = float(input("Ingrese la segunda nota: "))
 nota3 = float(input("Ingrese la tercera nota: "))
 
-promedio = (nota1 + nota2 + nota3) / 3
+notas = [nota1, nota2, nota3]
+
+promedio = calcular_promedio(notas)
 
 print("\n--- RESULTADOS ---")
 print("Estudiante:", nombre)
